33345bc1ee443384126bd18c65b64eadfb1feced
naveenwebdeveloper <85772223+naveenwebdeveloper@users.noreply.github.com>
2021-11-23 08:44:34 +0530

    first commit

---
+++ src/Index-comp/sliderComp1.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function sliderComp1() {
+    return (
+        <>
+            <section class="carousel-slider-main text-center border-top border-bottom bg-white">
+                <div class="owl-carousel owl-carousel-slider">
+                    <div class="item">
+                        <a href="shop.html"><img class="img-fluid" src="img/slider/slider1.jpg" alt="First slide" /></a>
+                    </div>
+                    <div class="item">
+                        <a href="shop.html"><img class="img-fluid" src="img/slider/slider2.jpg" alt="First slide" /></a>
+                    </div>
+                    <div class="item">
+                        <a href="shop.html"><img class="img-fluid" src="img/slider/slider1.jpg" alt="First slide" /></a>
+                    </div>
+                    <div class="item">
+                        <a href="shop.html"><img class="img-fluid" src="img/slider/slider2.jpg" alt="First slide" /></a>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
